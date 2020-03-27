4bcc2cee0fa8d79cfd2f05c0079eaa12f546f4f1
Kyle <brooks.kyle621@gmail.com>
2020-03-27 13:56:08 +0200

    Got basic login layout. Want to add image and container with color overlay to a stack to add background

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,188 +16,168 @@ class LoginScreenState extends State<LoginScreen> {
         decoration: BoxDecoration(
           color: Colors.white,
         ),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
+        constraints: BoxConstraints.expand(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
-                ),
-                Image(
-                  image: AssetImage('assets/images/logo.png'),
-                  height: 100,
-                  width: 100,
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Container(
-                      width: MediaQuery
-                          .of(context)
-                          .size
-                          .width * 0.8,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(color: Color.fromARGB(20, 0, 0, 0)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 4.0, horizontal: 10.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              hintText: 'Email', border: InputBorder.none),
+            SizedBox(
+              height: 80,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Image(
+                          image: AssetImage('assets/images/logo.png'),
+                          height: 100,
+                          width: 100,
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Container(
-                      width: MediaQuery
-                          .of(context)
-                          .size
-                          .width * 0.8,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(color: Color.fromARGB(20, 0, 0, 0)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 4.0, horizontal: 10.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              hintText: 'Password', border: InputBorder.none),
-                          obscureText: true,
+                        SizedBox(
+                          height: 50,
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Container(
-                      width: MediaQuery
-                          .of(context)
-                          .size
-                          .width * 0.8,
-                      decoration: BoxDecoration(
-                        color: Colors.blue[400],
-                        border: Border.all(color: Color.fromARGB(20, 0, 0, 0)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 15.0, horizontal: 10.0),
-                        child: Text(
-                          'Login',
-                          style: TextStyle(
+                        // Text('data', style: TextStyle(),textAlign: TextAlign.start,),
+                        // SizedBox(height:20),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          decoration: BoxDecoration(
                             color: Colors.white,
-                            fontWeight: FontWeight.bold,
+                            border: Border(
+                                bottom: BorderSide(
+                                    color: Color.fromARGB(80, 0, 0, 0))),
+                            // borderRadius: BorderRadius.circular(10),
                           ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width * 0.8,
-                  child: Row(children: <Widget>[
-                    Expanded(child: Divider()),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Text(
-                      "OR",
-                      style: TextStyle(
-                        color: Color.fromARGB(120, 0, 0, 0),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Expanded(child: Divider()),
-                  ]),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Container(
-                      width: MediaQuery
-                          .of(context)
-                          .size
-                          .width * 0.8,
-                      decoration: BoxDecoration(
-                        color: Colors.red[400],
-                        border: Border.all(color: Color.fromARGB(20, 0, 0, 0)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 8.0,
-                          horizontal: 10.0,
-                        ),
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              FontAwesomeIcons.google,
-                              size: 30.0,
-                              color: Colors.white70,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: 'Email', border: InputBorder.none),
                             ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border(
+                                bottom: BorderSide(
+                                    color: Color.fromARGB(80, 0, 0, 0))),
+                            // borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: 'Password',
+                                  border: InputBorder.none),
+                              obscureText: true,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          decoration: BoxDecoration(
+                            color: Colors.blue[400],
+                            border:
+                                Border.all(color: Color.fromARGB(20, 0, 0, 0)),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 15.0, horizontal: 10.0),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: Row(children: <Widget>[
+                            Expanded(child: Divider()),
                             SizedBox(
-                              width: 10,
+                              width: 20,
                             ),
                             Text(
-                              'Login with Google',
+                              "OR",
                               style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
+                                color: Color.fromARGB(120, 0, 0, 0),
+                              ),
                             ),
-                          ],
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Expanded(child: Divider()),
+                          ]),
                         ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          decoration: BoxDecoration(
+                            color: Colors.red[400],
+                            border:
+                                Border.all(color: Color.fromARGB(20, 0, 0, 0)),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 8.0,
+                              horizontal: 10.0,
+                            ),
+                            child: Row(
+                              children: <Widget>[
+                                Icon(
+                                  FontAwesomeIcons.google,
+                                  size: 30.0,
+                                  color: Colors.white70,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Login with Google',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ]),
+                  Container(
+                    child: Text(
+                      'Not a member yet? Sign up',
+                      style: TextStyle(
+                        color: Colors.blue,
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(),
-                Text(
-                  'Not a member yet? Sign up',
-                  style: TextStyle(
-                    color: Colors.blue,
                   ),
-                )
-              ],
+                ],
+              ),
             ),
             SizedBox(
-              height: 10,
+              height: 20,
             ),
-
           ],
         ),
       ),
